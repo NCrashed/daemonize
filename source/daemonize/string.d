9eaea8884e837a153a977ca36fe48ee6dbf73d6a
Anton Gushcha <NCrashed@gmail.com>
2016-10-01 16:42:56 +0400

    Fix #8
    Fix license block, relicensed to MIT

--- source/daemonize/string.d
+++ source/daemonize/string.d
@@ -1,22 +1,6 @@
 // written in the D programming language
-/*
-*   This file is part of DrossyStars.
-*   
-*   DrossyStars is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*   
-*   DrossyStars is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*   
-*   You should have received a copy of the GNU General Public License
-*   along with DrossyStars.  If not, see <http://www.gnu.org/licenses/>.
-*/
 /**
-*   Copyright: © 2014 Anton Gushcha
+*   Copyright: © 2014-2016 Anton Gushcha
 *   License: Subject to the terms of the MIT license, as written in the included LICENSE file.
 *   Authors: Anton Gushcha <ncrashed@gmail.com>
 */
